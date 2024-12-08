de8df4137660ab861b112433f1e55d3f4c2d3c6d
Muhammad Ammar Ayyasy <92962130+ammarayyasy@users.noreply.github.com>
2024-12-08 21:32:56 +0700

    Update cloudStorage.js

--- utils/cloudStorage.js
+++ utils/cloudStorage.js
@@ -3,11 +3,11 @@ const { Storage } = require('@google-cloud/storage');
 const tf = require('@tensorflow/tfjs-node');
 
 const storage = new Storage();
-const bucketName = process.env.CLOUD_STORAGE_BUCKET_NAME;
+// const bucketName = process.env.CLOUD_STORAGE_BUCKET_NAME;
 
 const loadCloudStorageModel = async () => {
   try {
-    const modelUrl = `https://storage.googleapis.com/${bucketName}/model/model.json`;
+    const modelUrl = `https://storage.googleapis.com/cancer-bucker-ade/model/model.json`;
     console.log(`Loading model from URL: ${modelUrl}`);
     const model = await tf.loadGraphModel(modelUrl);
     console.log('Model loaded successfully');
